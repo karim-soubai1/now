e64972f520f3cb14c0040a5e3aee81ffc7be0f9a
karim-soubai1 <karim1-12345@hotmai.com>
2017-01-08 22:15:43 +0000

    datatype-floatpoint
    this is program for float type

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 1f / 3;
+            double b = 1d / 3;
+            decimal c = 1m / 3;
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+            Console.WriteLine("c=" + c);
+            
+
+
+            float m = float.MinValue;
+            float x = float.MaxValue;
+            Console.WriteLine("max:{0} \n min:{1}",m,x);
+            Console.ReadLine();
+        }
+    }
+}
