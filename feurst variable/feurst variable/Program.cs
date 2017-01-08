abc8b4038a83a0cde28391fbf6e39247347b7f30
karim-soubai1 <karim1-12345@hotmai.com>
2017-01-08 12:46:25 +0000

    karim
    solitin

---
+++ feurst variable/feurst variable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feurst_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            Console.WriteLine("le variable est{0}",x);
+            Console.ReadKey();
+        }
+    }
+}
