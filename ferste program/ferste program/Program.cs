72de9beeae98d2d4d9e28f0014891de0137b82d7
karim-soubai1 <karim1-12345@hotmai.com>
2017-01-08 12:55:31 +0000

    ferstprogram
    now h kan do this

---
+++ ferste program/ferste program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ferste_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hallo word");
+            Console.ReadKey();
+        }
+    }
+}
