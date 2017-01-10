e801a2233bba06743feaff879208061f1e232afb
karim-soubai1 <karim1-12345@hotmai.com>
2017-01-10 21:33:31 +0000

    data
    data baze

---
+++ datatype/datatype/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datatype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = char.MinValue;
+            int b = char.MaxValue;
+
+            char c = 'k';
+            int  d = 'k';
+            char e = Convert.ToChar(d);
+
+
+
+            Console.WriteLine("max char variabal = {0} \nmin char varibal = {1}\nle caracter est:{2}\nle nombre de caracter est:{3}\nle caracter={4}"
+                ,b,a,c,d,e);
+
+
+            string f = "my string  valio";
+            bool g=true;
+            Console.WriteLine("{0}\nmy booling vribal is:{1}", f,g);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
